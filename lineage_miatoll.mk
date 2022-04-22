3c745ca70aa123b3d90d6cd262035386c6c5a4ce
Mohammad Nt <mohammadnt96@gmail.com>
2022-04-22 19:02:04 +0430

    add common_full_phone.mk to lineage_miatoll.mk

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -4,11 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/lineage/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
