701f2ff8df3ce61a1a8167edeca17b964e58b6a0
theSkullx <theskull641@gmail.com>
2024-04-26 23:25:56 +0530

    miatoll: Move battery cycle count to settings overlay

--- device.mk
+++ device.mk
@@ -306,6 +306,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     NotchBarKiller \
+    SettingsOverlay \
     SM6250Frameworks \
     SM6250SystemUI \
     TelephonyResCommon \
