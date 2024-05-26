8890ab34ac9afb891b9700900e1c73648e1229a7
theskullx <theskull641@gmail.com>
2024-05-26 18:16:54 +0530

    miatoll: Declare some hals in FCM The following HALs in device manifest are not declared in FCM <= level 5:   android.hardware.biometrics.fingerprint@2.3::IBiometricsFingerprint/default   android.hardware.bluetooth.audio.IBluetoothAudioProviderFactory/default (@4)   android.hardware.cas.IMediaCasService/default (@1)   android.hardware.drm.IDrmFactory/clearkey (@1)   android.hardware.health.IHealth/default (@1)   android.hardware.light.ILights/default (@2)   android.hardware.power.IPower/default (@4)   android.hardware.thermal.IThermal/default (@2)   android.hardware.usb@1.3::IUsb/default   android.hardware.vibrator.IVibrator/default (@2)   android.hardware.wifi.IWifi/default (@2)   android.hardware.wifi.hostapd.IHostapd/default (@2)   android.hardware.wifi.supplicant.ISupplicant/default (@3)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,7 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    $(DEVICE_PATH)/device_framework_matrix.xml \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/lineage_device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
