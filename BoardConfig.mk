40e202b816641149eab87d64ce48212d74d8c220
theSkullx <theskull641@gmail.com>
2024-06-09 18:46:35 +0530

    miatoll: Add path for public sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,6 +193,7 @@ VENDOR_SECURITY_PATCH := 2023-05-01
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
