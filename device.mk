56056f38b4af49a13f83b386cb9e279ee7b23c42
@bgcngm <1696148+bgcngm@users.noreply.github.com>
2024-06-30 16:49:42 +0530

    miatoll: Convert WiFi firmware symlinks to install_symlink targets The last bit that was preventing building hermetic images.
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- device.mk
+++ device.mk
@@ -528,6 +528,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# WiFi Firmware Symlinks
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_symlink
+
 # Compile SystemUI on device with `speed`.
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.systemuicompilerfilter=speed
