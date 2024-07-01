c28063b7d225087cdd8e2a0aaad1e3efa727da8b
theSkullx <theskull641@gmail.com>
2024-07-01 22:28:02 +0530

    miatoll: Remove System UI compiler filter Already done in vendor_crdroid

--- device.mk
+++ device.mk
@@ -533,10 +533,5 @@ PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
-# Compile SystemUI on device with `speed`.
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.systemuicompilerfilter=speed
-
-
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
