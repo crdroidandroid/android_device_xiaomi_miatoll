31e87fc99aafef7886312ba8ab9d7d76152aef0b
johnmart19 <johnivan19999@gmail.com>
2024-09-15 02:49:25 +0530

    miatoll: FCM: Import MiSys entries From Miui Camera repo it's not picked on AOSP
    stderr:ERROR: files are incompatible: The following instances are in the device manifest but not specified in framework compatibility matrix:
        vendor.xiaomi.hardware.misys@1.0::IMiSys/default
        vendor.xiaomi.hardware.misys@2.0::IMiSys/default
        vendor.xiaomi.hardware.misys@3.0::IMiSys/default
        vendor.xiaomi.hardware.misys@4.0::IMiSys/default
    
    - Second part of https://gitlab.com/johnmart19/vendor_xiaomi_camera/-/commit/3aae190940aeb1103a007a599aa4c9c16a03194b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
