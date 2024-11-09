7cd39dc4c37cf0fc2fbf04b7fbeb5cee74a0656e
Cyber Knight <cyberknight755@gmail.com>
2024-11-10 00:18:54 +0530

    miatoll: Keep building fingerprint HIDL interface lib
    - To retain compat with fingerprint.fpc.default.so

--- device.mk
+++ device.mk
@@ -119,6 +119,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 PRODUCT_PACKAGES += \
