2995b10b474e404120f7e0d3ed05845333d1173b
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    miatoll: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- miatoll.mk
+++ miatoll.mk
@@ -338,7 +338,6 @@ $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
-    libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     wpa_supplicant \
