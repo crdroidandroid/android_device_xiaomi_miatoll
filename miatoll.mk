58cfa1b47d84b9f8bd73aea3a1e53c5e3089de94
Bruno Martins <bgcngm@gmail.com>
2025-04-30 21:12:33 +0100

    miatoll: Set camera override format from reserved via soong config
    Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550

--- miatoll.mk
+++ miatoll.mk
@@ -57,6 +57,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
 # Camera
+$(call soong_config_set,camera,override_format_from_reserved,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
