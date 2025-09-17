04bc71dc7e0dc71ef51bc7163aa8554d7ee23fde
Ritesh <SahanyRitesh@proton.me>
2025-09-18 04:19:26 +0530

    miatoll: Build vendor sensorservice 1.0 HAL
    Change-Id: Idffa7c417400bc059b3d50fdbf5622951d9ab88e

--- miatoll.mk
+++ miatoll.mk
@@ -285,6 +285,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors-service.xiaomi-multihal
 
 # Shipping API level
