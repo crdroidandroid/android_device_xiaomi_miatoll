7799b7b654dde27862062d5b44d7e58407a0e94b
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-06-10 00:14:34 +0530

    miatoll: Include OEM Dolby App
    - Required after https://github.com/swiitch-OFF-Lab/hardware_dolby/commit/a29e40ab583f40893552d917c6d4deddac125b01
    
    Change-Id: Iac1cce16d3b41ae88f182773f50bca929b1f2656

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -30,3 +30,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="miatoll_global-user 12 SKQ1.211019.001 V14.0.3.0.SJZMIXM release-keys" \
     BuildFingerprint=Redmi/miatoll_global/miatoll:12/RKQ1.211019.001/V14.0.3.0.SJZMIXM:user/release-keys
+
+# Flags
+TARGET_INCLUDES_OEM_App := true
